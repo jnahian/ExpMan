90a8b4d8f5b7be11fd016037ffd1f877adbf7e23
Nahian <nahian.bigm@gmail.com>
2019-02-20 14:05:01 +0600

    middleware added

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,18 @@
 
 Route::get( '/', function () {
     return view( 'welcome' );
-} );
+} )->middleware( 'guest' );
 
 Auth::routes();
 
-Route::get( '/home', 'HomeController@index' )->name( 'home' );
-Route::resource( '/income', 'IncomeController' );
-Route::resource( '/expense', 'ExpenseController' );
+Route::group( [ 'middleware' => 'auth' ], function () {
+    Route::get( '/home', 'HomeController@index' )->name( 'home' );
+    Route::resource( '/income', 'IncomeController' );
+    Route::resource( '/expense', 'ExpenseController' );
 
-Route::group( [ 'prefix' => 'report', 'as' => 'report.' ], function () {
-    Route::get( '/', 'ReportsController@index' )->name( 'index' );
-    Route::get( '/daily', 'ReportsController@daily_income_expense' )->name( 'daily' );
-    Route::get( '/monthly', 'ReportsController@monthly_income_expense' )->name( 'monthly' );
+    Route::group( [ 'prefix' => 'report', 'as' => 'report.' ], function () {
+        Route::get( '/', 'ReportsController@index' )->name( 'index' );
+        Route::get( '/daily', 'ReportsController@daily_income_expense' )->name( 'daily' );
+        Route::get( '/monthly', 'ReportsController@monthly_income_expense' )->name( 'monthly' );
+    } );
 } );
